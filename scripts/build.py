2aab5a5fc750a9e17cfda87918d2db55b1eaa4f4
Joongi Kim <joongi@lablup.com>
2021-08-19 14:49:01 +0900

    fix: typo in docker build arg

--- scripts/build.py
+++ scripts/build.py
@@ -33,7 +33,7 @@ def main(distro, arch):
     click.secho(f'Bundling static Python for krunner for {distro}', fg='yellow', bold=True)
     subprocess.run([
         'docker', 'buildx', 'build',
-        '--arg', f'ARCH={arch}',
+        '--build-arg', f'ARCH={arch}',
         '--platform', f'linux/{arch}',
         '-f', f'krunner-python.{distro}.dockerfile',
         '-t', f'lablup/backendai-krunner-python:{distro}',
@@ -45,7 +45,7 @@ def main(distro, arch):
     subprocess.run([
         'docker', 'buildx', 'build',
         '--platform', f'linux/{arch}',
-        '--arg', f'ARCH={arch}',
+        '--build-arg', f'ARCH={arch}',
         '-f', f'krunner-env.{distro}.dockerfile',
         '-t', f'krunner-env.{distro}',
         '.'
